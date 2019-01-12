2676d72359444586a7ad7f3bce5d9254799758a6
Shubham Chauhan <44020728+shubhiamchauhan@users.noreply.github.com>
2019-01-12 14:15:02 +0530

    Made a ball div and ball class.
    and made it moving in a random direction.

--- controller.js
+++ controller.js
@@ -1,13 +1,30 @@
-const createPaddleDiv = function(document){
-  let div = document.createElement('div');
-  div.id = 'paddle_1';
-  div.className = 'paddle';
-  return div;
-}
-
 const applyPixel = (count) => count + "px";
 
-const drawPaddle = function(document, paddle){
+const createWallDiv = function (document) {
+  const main = document.getElementById('main');
+  const wallDiv = document.createElement('div');
+  wallDiv.id = 'screen';
+  wallDiv.tabIndex = '0';
+  wallDiv.className = 'board';
+  main.appendChild(wallDiv);
+}
+
+const drawWallDiv = function (document, wall) {
+  const screen = document.getElementById('screen');
+  screen.style.height = applyPixel(wall.height);
+  screen.style.width = applyPixel(wall.width);
+}
+
+const createPaddleDiv = function (document) {
+  const screen = document.getElementById('screen');
+  let paddleDiv = document.createElement('div');
+  paddleDiv.id = 'paddle_1';
+  paddleDiv.className = 'paddle';
+  screen.appendChild(paddleDiv);
+}
+
+
+const drawPaddle = function (document, paddle) {
   const paddleDiv = document.getElementById('paddle_1');
   paddleDiv.style.height = applyPixel(paddle.height);
   paddleDiv.style.width = applyPixel(paddle.width);
@@ -15,20 +32,52 @@ const drawPaddle = function(document, paddle){
   paddleDiv.style.bottom = applyPixel(paddle.bottom);
 }
 
-const move = function(document,paddle){
+const move = function (document, paddle) {
   console.log(event);
-  if(event.key == "ArrowRight") paddle.moveRight();
-  if(event.key == "ArrowLeft") paddle.moveLeft();
+  if (event.key == "ArrowRight") paddle.moveRight();
+  if (event.key == "ArrowLeft") paddle.moveLeft();
   drawPaddle(document, paddle);
 }
 
-const initialise = function(){
+const drawBall = function (document, ball) {
+  const ballDiv = document.getElementById('ball');
+  ballDiv.style.height = applyPixel(ball.radius * 2);
+  ballDiv.style.width = applyPixel(ball.radius * 2);
+  ballDiv.style.left = applyPixel(ball.left);
+  ballDiv.style.bottom = applyPixel(ball.bottom);
+}
+
+const createBallDiv = function (document) {
+  const screen = document.getElementById('screen');
+  let ballDiv = document.createElement('div');
+  ballDiv.className = 'circle';
+  ballDiv.id = 'ball';
+  screen.appendChild(ballDiv);
+}
+
+const createElements = function (document, paddle, ball) {
+  const wall = new Wall(570, 800);
+  createWallDiv(document);
+  createPaddleDiv(document);
+  createBallDiv(document);
+  drawWallDiv(document, wall);
+  drawPaddle(document, paddle);
+  drawBall(document, ball);
+}
+
+const initialise = function () {
   const paddle = new Paddle(20, 100, 350, 1, 15);
-  const paddleDiv = createPaddleDiv(document);
-  let screen = document.getElementById('screen');
-  screen.appendChild(paddleDiv);
-  drawPaddle(document, paddle);
+  const velocity = new Velocity(1, 1);
+  const ball = new Ball(20, 390, 22, velocity);
+  createElements(document, paddle, ball);
+  const screen = document.getElementById('screen');
   screen.onkeydown = move.bind(null, document, paddle);
+  setInterval(() => {
+    ball.moveBall()
+    drawBall(document, ball);
+  }, 10)
 }
 
-window.onload = initialise;
+window.onload = () => {
+  initialise();
+}
--- game.js
+++ game.js
@@ -1,5 +1,12 @@
+class Wall {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+}
+
 class Paddle {
-  constructor(height, width, left, bottom, speed){
+  constructor(height, width, left, bottom, speed) {
     this.height = height;
     this.width = width;
     this.left = left;
@@ -7,11 +14,41 @@ class Paddle {
     this.speed = speed;
   }
 
-  moveLeft(){
+  moveLeft() {
     this.left = this.left - this.speed;
   }
 
-  moveRight(){
+  moveRight() {
     this.left = this.left + this.speed;
+  }
+}
+
+
+class Velocity {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  negateX() {
+    this.x = -1 * this.x;
+  }
+
+  negateY() {
+    this.y = -1 * this.y;
+  }
+}
+
+class Ball {
+  constructor(radius, left, bottom, velocity) {
+    this.radius = radius;
+    this.left = left;
+    this.bottom = bottom;
+    this.velocity = velocity;
+  }
+
+  moveBall() {
+    this.left += this.velocity.x;
+    this.bottom += this.velocity.y;
   }
 }
